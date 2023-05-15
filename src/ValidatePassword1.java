197f4780d0eef77d7a87d50836fb86d23684621c
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-15 12:35:38 +0530

    Adding a program for validating Password in smallcase

---
+++ src/ValidatePassword1.java
@@ -0,0 +1,46 @@
+package com.regex;
+
+import java.util.*;
+
+import java.util.regex.Pattern;
+
+import java.util.regex.Matcher;
+/**
+ * 
+ * @author DELL
+ *
+ */
+
+public class ValidatePassword1 {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * Declaring Scanner Object
+         * Define regex for password and matching password name with the give expression
+		 * Displaying the result
+		 */
+	
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the password");
+
+		String password = sc.next();
+
+		/*
+		 * To define regex for password according to rules and matching password with expression given by the user
+		 */
+		boolean result = Pattern.matches("[a-z]{8,}", password);
+
+		if (result) {
+			System.out.println("Password entered by the user is valid");
+		} else {
+			System.out.println("Password  entered by the user is invalid");
+		}
+
+	}
+
+}
